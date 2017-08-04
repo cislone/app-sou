161e00f91f8ed2a765891d19bb226efc5d92f24d
slone <s_lone@qq.com>
2017-08-05 01:06:11 +0800

    no message

--- js/txdm.js
+++ js/txdm.js
@@ -1,6 +1,6 @@
 javascript:void(function() {
 	try {
-		var elms = getElementsByClassName("app-guiding-download");
+		var elms = document.getElementsByClassName("app-guiding-download");
 		for (var i = 0; i < elms.length; i++) {
             elms[i].parentNode.removeChild(elms[i]);    
         }
