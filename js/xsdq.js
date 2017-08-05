e2967bd514e715ae2b2f237804b0893452868621
cislone <s_lone@qq.com>
2017-08-05 21:03:49 +0800

    Update xsdq.js

--- js/xsdq.js
+++ js/xsdq.js
@@ -1,6 +1,18 @@
 javascript:void(function() {
 	try {
 		var elms = document.getElementsByClassName("header");
-    elms[0].parentNode.removeChild(elms[0]);
+                elms[0].parentNode.removeChild(elms[0]);
+		
+		var elm = document.getElementById("top_app_download");
+		elm.parentNode.removeChild(elm);
+		
+		var elms = document.getElementsByClassName("img-banner");
+                elms[0].parentNode.removeChild(elms[0]);
+		
+		var elms = document.getElementsByClassName("search-form");
+                elms[0].parentNode.removeChild(elms[0]);
+		
+		var elms = document.getElementsByClassName("public-module");
+                elms[elms.length - 1].parentNode.removeChild(elms[elms.length - 1]);
 	} catch(e) {}
 }())
